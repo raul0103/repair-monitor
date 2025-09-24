244cabaa9606c577a239dbef18a03fa5322fcb6e
raul0103 <rshakurov95@mail.ru>
2025-09-24 15:20:41 +0500

    migrations

---
+++ app/database/migrations/2025_09_24_101454_create_request_comments.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('request_comments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('request_id');
+            $table->text('text');
+            $table->date('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('request_comments');
+    }
+};
